9a2bbe10dbb58cbea7e1ae8433c56bd663fbb6c0
JeremyJS <77928002+JeremyJS-hub@users.noreply.github.com>
2021-03-30 20:32:43 -0400

    ...

--- Scenes/scenes.js
+++ Scenes/scenes.js
@@ -5,198 +5,184 @@
 var vGlobales = new gVariables();
 
 var inicio = new Audio('audio/inicio1.mp3');
-var menu_pausa = new Audio('audio/menu_pausa.ogg');
-var explosion_nave = new Audio('audio/explosion_nave.mp3');
+var menu_pausa = new Audio('audio/menu_pausa.ogg'); 
+var executed;
+
 
 //Escenas o vistas del juego
 
 //Vista de pausa
 class sceneMenuPause extends Phaser.Scene {
-    constructor() {
-        super({ key: 'sceneMenuPause' })
-    }
+  constructor() {
+      super({ key: 'sceneMenuPause' })
+  }
 
-    init(data) {
+  init(data) {
 
-    }
+  }
 
-    preload() {}
+  preload() {}
 
-    create() {
-        this.add.text(500, 550, "Juego Pausado", {
-            color: "#000",
-            backgroundColor: "#fff",
-            fontSize: 25,
-            padding: 10,
-        });
+  create() {
+      this.add.text(500, 550, "Juego Pausado", {
+          color: "#000",
+          backgroundColor: "#fff",
+          fontSize: 25,
+          padding: 10,
+      });
 
-        stopAudio(inicio);
-        PlayAudio(menu_pausa);
-        resume = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P);
+      stopAudio(inicio);
+      PlayAudio(menu_pausa);
+      resume = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P);
 
-        resume.on("down", (event) => {
-            stopAudio(menu_pausa);
-            PlayAudio(inicio);
-            this.scene.stop();
-            this.scene.resume("scenePlayGame");
-        });
-    }
+      resume.on("down", (event) => {
+          stopAudio(menu_pausa);
+          PlayAudio(inicio);
+          this.scene.stop();
+          this.scene.resume("scenePlayGame");
+      });
+  }
 
-    update(timer, delta) {}
+  update(timer, delta) {}
+  
 }
 
 //Vista de juego ganado
 class sceneWon extends Phaser.Scene {
-    constructor() {
-        super({ key: 'sceneWon' })
-    }
+  constructor() {
+      super({ key: 'sceneWon' })
+  }
 
-    init(data) {
-        this.time = data.time;
-        this.score = data.score;
-    }
+  init(data) {
+      this.time = data.time;
+      this.score = data.score;
+  }
 
-    preload() {}
+  preload() {}
 
-    create() {
-        this.add.text(500, 350, "Has ganado", {
-            color: "#000",
-            backgroundColor: "#fff",
-            fontSize: 25,
-            padding: 10,
-        });
-        this.add.text(500, 450, "Puntuacion: " + this.score, {
-            color: "#000",
-            backgroundColor: "#fff",
-            fontSize: 25,
-            padding: 10,
-        });
-        this.add.text(500, 550, "Tiempo: " + this.time, {
-            color: "#000",
-            backgroundColor: "#fff",
-            fontSize: 25,
-            padding: 10,
-        });
-    }
+  create() {
+      this.add.text(500, 350, "Has ganado", {
+          color: "#000",
+          backgroundColor: "#fff",
+          fontSize: 25,
+          padding: 10,
+      });
+      this.add.text(500, 450, "Puntuacion: " + this.score, {
+          color: "#000",
+          backgroundColor: "#fff",
+          fontSize: 25,
+          padding: 10,
+      });
+      this.add.text(500, 550, "Tiempo: " + this.time, {
+          color: "#000",
+          backgroundColor: "#fff",
+          fontSize: 25,
+          padding: 10,
+      });
+  }
 
-    update(timer, delta) {}
+  update(timer, delta) {}
 }
 
 //Vista de juego perdido
 class sceneLose extends Phaser.Scene {
-    constructor() {
-        super({ key: 'sceneLose' })
-    }
+  constructor() {
+      super({ key: 'sceneLose' })
+  }
 
-    init(data) {
-        this.reasonLose = data.rl;
-        this.time = data.time;
-        this.score = data.score;
-    }
+  init(data) {
+      this.reasonLose = data.rl;
+      this.time = data.time;
+      this.score = data.score;
+  }
 
-    preload() {}
+  preload() {}
 
-    create() {
-        this.add.text(
-            400,
-            350,
-            "La nave a explotado por \n" +
-            this.reasonLose +
-            "\nPresione ENTER para volver a jugar", {
-                color: "#000",
-                backgroundColor: "#fff",
-                fontSize: 20,
-                padding: 10,
-            }
-        );
-        this.add.text(500, 450, "Puntuacion: " + this.score, {
-            color: "#000",
-            backgroundColor: "#fff",
-            fontSize: 25,
-            padding: 10,
-        });
-        this.add.text(500, 550, "Tiempo: " + this.time, {
-            color: "#000",
-            backgroundColor: "#fff",
-            fontSize: 25,
-            padding: 10,
-        });
+  create() {
+      this.add.text(
+          400,
+          350,
+          "La nave a explotado por \n" +
+          this.reasonLose +
+          "\nPresione ENTER para volver a jugar", {
+              color: "#000",
+              backgroundColor: "#fff",
+              fontSize: 20,
+              padding: 10,
+          }
+      );
+      this.add.text(500, 450, "Puntuacion: " + this.score, {
+          color: "#000",
+          backgroundColor: "#fff",
+          fontSize: 25,
+          padding: 10,
+      });
+      this.add.text(500, 550, "Tiempo: " + this.time, {
+          color: "#000",
+          backgroundColor: "#fff",
+          fontSize: 25,
+          padding: 10,
+      });
 
-        PlayAudio(explosion_nave);
-        PlayAudio(menu_pausa);
-        stopAudio(inicio);
-        resume = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
+      PlayAudio(menu_pausa);
+      stopAudio(inicio);
+      resume = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
 
-        resume.on("down", (event) => {
+      resume.on("down", (event) => {
 
-            PlayAudio(inicio);
-            stopAudio(menu_pausa);
-            bullet2 = null
-            this.scene.stop();
-            this.scene.stop('scenePlayGame');
+          PlayAudio(inicio);
+          stopAudio(menu_pausa);
+          bullet2 = null
+          this.scene.stop();
+          this.scene.stop('scenePlayGame');
 
-            this.scene.start("scenePlayGame", {
-                bg: bgScenes[Math.floor(Math.random() * (4 - 0)) + 0]
-            });
-        });
-    }
+          this.scene.start("scenePlayGame", {
+              bg: bgScenes[Math.floor(Math.random() * (4 - 0)) + 0]
+          });
+      });
+  }
 
-    update(timer, delta) {}
+  update(timer, delta) {}
 }
-
-// resu = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-
-// resume.on("down", (event) => {
-//     console.log("probandoooo");
-//     PlayAudio(inicio);
-//     stopAudio(menu_pausa);
-// bullet2 = null
-// this.scene.stop();
-// this.scene.stop('scenePlayGame');
-
-// this.scene.start("scenePlayGame", {
-//     bg: bgScenes[Math.floor(Math.random() * (4 - 0)) + 0]
-// });
-// });
 
 //Vista de comenzar juego
 class sceneStartGame extends Phaser.Scene {
-    constructor() {
-        super({ key: 'sceneStartGame' })
-    }
-    preload() {}
+  constructor() {
+      super({ key: 'sceneStartGame' })
+  }
+  preload() {}
 
-    create() {
-        this.add.text(400, 350, "Presione ENTER para \n comenzar juego", {
-            color: "#000",
-            backgroundColor: "#fff",
-            fontSize: 20,
-            padding: 10,
-        });
+  create() {
+      this.add.text(400, 350, "Presione ENTER para \n comenzar juego", {
+          color: "#000",
+          backgroundColor: "#fff",
+          fontSize: 20,
+          padding: 10,
+      });
 
-        resume = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
-        PlayAudio(menu_pausa);
+      resume = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
+      PlayAudio(menu_pausa);
 
-        resume.on("down", (event) => {
+      resume.on("down", (event) => {
 
-            PlayAudio(inicio);
-            stopAudio(menu_pausa);
-            this.scene.start("scenePlayGame", {
-                bg: bgScenes[Math.floor(Math.random() * (4 - 0)) + 0]
-            });
-        });
-    }
-    update(timer, delta) {}
+          PlayAudio(inicio);
+          stopAudio(menu_pausa);
+          this.scene.start("scenePlayGame", {
+              bg: bgScenes[Math.floor(Math.random() * (4 - 0)) + 0]
+          });
+      });
+  }
+  update(timer, delta) {}
 }
 
 function PlayAudio(audio) {
-    audio.play();
-    audio.currentTime = 0;
+  audio.play();
+  audio.currentTime = 0;
 }
 
 function stopAudio(audio) {
-    audio.pause();
-    audio.currentTime = 0;
+  audio.pause();
+  audio.currentTime = 0;
 }
 
-export { sceneMenuPause as default, sceneWon, sceneLose, sceneStartGame };
+export { sceneMenuPause as default, sceneWon, sceneLose, sceneStartGame }; 
